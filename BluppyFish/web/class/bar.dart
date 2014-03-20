0b8ddeba942933bd5f9ddb338f3e7668dcdd3120
rbreton <bretonr@gmail.com>
2014-03-20 18:39:56 -0400

    ADD bar + pointage

---
+++ BluppyFish/web/class/bar.dart
@@ -0,0 +1,15 @@
+part of BluppyFish;
+
+class Bar {
+  Board board;
+  num x;
+  num y;
+
+  Bar(this.board, this.x, this.y) {
+    draw();
+  }
+
+  void draw() {
+
+  }
+}
---
+++ BluppyFish/web/class/pointage.dart
@@ -0,0 +1,15 @@
+part of BluppyFish;
+
+class Pointage {
+  Board board;
+  num x;
+  num y;
+
+  Pointage(this.board, this.x, this.y) {
+    draw();
+  }
+
+  void draw() {
+
+  }
+}
