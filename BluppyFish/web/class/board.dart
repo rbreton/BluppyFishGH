e7823b9a8a43fce2f259db3377fda4a5b635494e
rbreton <bretonr@gmail.com>
2014-03-28 14:04:11 -0400

    v01

--- BluppyFish/web/class/board.dart
+++ BluppyFish/web/class/board.dart
@@ -6,8 +6,7 @@ class Board {
 
   CanvasElement canvas;
   CanvasRenderingContext2D context;
-  bool etatGame = true;
-  int timeOutStart = 0;
+  bool etatGame = false;
 
   num width;
   num height;
@@ -21,7 +20,9 @@ class Board {
   Bouton bouton;
   List lstBars = new List(3);
   
-
+  var mouseUpListen;
+  var mouseDownListen;
+  /* est-ce que j'ai bien fait d'utiliser un objet html pour le bouton restart?*/
   Board(this.canvas) {
     context = canvas.getContext("2d");
     width = canvas.width;
@@ -29,7 +30,11 @@ class Board {
     border();
     init();
   }
-  void init() {
+  void init(){
+    mouseUpListen = document.querySelector('#btnNav').onMouseDown.listen(onMouseUp);
+  }
+  void initGame() {
+    document.querySelector('#btnNav').classes.add("hidden");
     bluppy = new Bluppy(this, 100, height / 2);
     pointage = new Pointage(this, 420, 20, 0);
     bouton = new Bouton(this, 200, 200);
@@ -37,31 +42,31 @@ class Board {
       lstBars[intI] = new Bar(this, (600+(intI*200)));
     }
     window.animationFrame.then(gameLoop);
-    document.onMouseDown.listen(onMouseDownBoost);
+    mouseDownListen = document.querySelector('#canvas').onMouseDown.listen(onMouseDownBoost);
   }
   void gameLoop(num delta) {
     if(etatGame) {
-      //if(timeOutStart >= 100){
         redraw();
-      //}
       window.animationFrame.then(gameLoop);
-      
     }
-    //timeOutStart++;
-    //print(timeOutStart);
   }
   void border() {
     context.beginPath();
     context.rect(X, Y, width, height);
     context.fillStyle = "#0008db";
-    context.fillText('Bluppy Fish', 150, 100); // ---- voir comment faire pour qu'il reste seulement un temps donnee
+    context.fillText('Bluppy Fish', 150, 100);
     context.closePath();
     context.stroke();
   }
   void clear() {
     context.clearRect(X, Y, width, height);
     border();
-  }  
+  }
+  void onMouseUp(MouseEvent e){
+    if(!etatGame){
+      restartGame(e);
+    }
+  }
   void onMouseDownBoost(MouseEvent e){
     numGY = 0;
     numStartGY = bluppy.y;
@@ -83,33 +88,33 @@ class Board {
       if(lstBars[intJ].x <= -30){
         lstBars[intJ] = new Bar(this, 600); 
       }
-      if(lstBars[intJ].x <= 110 && lstBars[intJ].x >= 85){
+      if(lstBars[intJ].x <= 110 && lstBars[intJ].x >= 75){
         if(bluppy.y >= lstBars[intJ].Y1 && bluppy.y <= 590-lstBars[intJ].Y2){
-          if(!lstBars[intJ].etatCpt){
+          if(!lstBars[intJ].etatCpt && lstBars[intJ].x <= 77){
             lstBars[intJ].etatCpt = true;
             pointage.point++;
           }
         }else{
-          print('hit');
           endGame();
         }
       }
     }
     pointage.draw();
     bluppy.x += numGX;
-    bluppy.y += numGY;
-    //Detection
-    
+    bluppy.y += numGY;    
   }
   void restartGame(MouseEvent e){
     etatGame = true;
-    init();
-    
+    initGame(); 
   }
   void endGame(){
     etatGame = false;
+    document.querySelector('#btnNav').innerHtml = 'Restart';
+    document.querySelector('#btnNav').classes.remove("hidden");
+    numGY = 0;
     bouton.draw();
-    //bouton.on.mouseUp.add((MouseEvent event) => restartGame(event));
+    mouseDownListen.cancel();
+    //bouton.addListeners();
     print('End');
   }
 }
